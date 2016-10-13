3f0e194c1d74f46edac8e0852de81b9c4cd6e401
sgnl <rayrfarias@gmail.com>
2016-10-13 13:14:48 -1000

    changes tape to ava; refactors tests

--- story_teller/character-adventure.test.js
+++ story_teller/character-adventure.test.js
@@ -1,34 +1,37 @@
 
-const Test = require('tape')
+const Test = require('ava')
 const VM = require('vm')
 const FS = require('fs')
 
-const CharacterFile = FS.readFileSync('./character.js')
+const CharacterFile = FS.readFileSync('../character.js')
 const script = new VM.Script(CharacterFile)
 script.runInThisContext()
 
-Test('Hero', t => {
-  t.equal(typeof hero, 'object', 'is the correct data type: Object')
+// check Hero attributes
+Test.serial('Hero is prepared for adventure', t => {
+  t.plan(7)
 
-  let correctKeys = [
+  t.pass('Your Hero is prepared for adventure!');
+  t.is(typeof hero, 'object', 'is the correct data type: Object')
+
+  let correctKey = [
     'name',
     'health',
     'age',
     'bag',
     'equipped'
-  ]
+  ].forEach(key => {
+    let hasKey = hero.hasOwnProperty(key)
+    t.true(hasKey, `missing property on hero: ${key}`)
+  })
 
-  Object
-    .keys(hero)
-    .forEach(key => {
-      t.true(correctKeys.includes(key), `has proper key: ${key}`)
-    })
-  t.comment('Your Hero is prepared for adventure!')
-  t.end()
 })
 
-Test('Something in the woods...', t => {
-  t.comment('You encounter a FangBat in the woods just outside of town')
+Test.serial('Something in the woods: FangBat', t => {
+  t.plan(4)
+
+  t.pass('You encounter a FangBat in the woods just outside of town')
+
   const EnemyBat = {
     type: 'bat',
     power: 10,
@@ -38,15 +41,15 @@ Test('Something in the woods...', t => {
   if (hero.health <= EnemyBat.power) {
     t.fail(`${hero.name} took too much damage from FangBat's ${EnemyBat.weapon}`)
   } else {
-    t.comment('You survived an EnemyBat encounter in the woods.')
+    t.pass('You survived an EnemyBat encounter in the woods.')
   }
 
   if (hero.bag.includes('potion')) {
-    t.comment('Your hero uses a potion to heal')
-    hero.bag.splice(hero.bag.indexOf('potion'), 1)
+    t.pass('Your hero uses a cure poison potion to heal')
+    hero.bag.splice(hero.bag.indexOf('cure poison potion'), 1)
   } else {
-    t.fail(`${heo.name} cannot heal and returns to town`)
+    t.fail(`${hero.name} cannot heal and returns to town`)
   }
+
   t.pass('you complete the first adventure')
-  t.end()
 })
