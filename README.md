e498a6d1cb7f0844a95900ce037190396450d2c9
yukio yamamoto <aj.yamamoto@me.com>
2016-10-20 18:37:15 -1000

    Finished comments instructions and tests

---
+++ README.md
@@ -0,0 +1,73 @@
+# JS Hero
+Welcome hero. To the world of programming, magic, and stuff.
+You have been invited to a world that has been ravaged for centuries by the gods that have tried and tried again to thwart that which is JS in to the dark ages. You oh great hero must now challenge the gods and save the world. Use your JS skills to end their centuries of terror and save the JS people.
+
+### firstName
+create a variable called `firstName` with your first name as a value.   
+`Hint: "these" of 'these usually help'.`
+
+### lastName
+create a variable called `lastName` with your last name as a value.   
+`Hint: 4 lines above.`
+
+### age
+create a variable called `age` with your current age.   
+`Hint: Its a number`
+
+### bag
+create a variable called `bag` with 2 items: `armor` and `map`.   
+`Hint: Its a type of list. Everything between time and space began at 0`.
+
+### heroClass
+create a variable called `heroClass` that will be one of 6 options. You will determine what class you want to be and work with.
+* `Archer`
+* `Swordsman`
+* `Sorcerous`
+* `thief`
+* `alchemist`
+* `No Class`    
+
+`Hint: 1 and 2.`
+
+### hero
+create a variable called hero. This will be your `profile` for the rest of the tests. You will need set different values that can be referenced by the `gods` aka the program.
+You will need to have these **`Properties`**:
+* `name`: Your first and last name separated with a space.
+* `health`: Set the value of your heath to be half your age.
+* `age`: Your age.
+* `bag`: Your bag that you created earlier.
+* `armorEquip`: Set it to `null` not in quotes.
+* `weaponEquip`: Set it to `null` not in quotes.
+* `heroClass`: Set it to your hero class.   
+`Hint: key value pairs`
+
+------
+## Logic
+Hero you have now taken your first step into the challenges that await you. Set off into the world where puzzles and monsters alike that the gods have thrown in your path and finish the quest.
+
+Logic will be needed here to combat the obstacles that will test the `conditions` of your status and history.
+
+### The Fanged Bat
+Oh no the winged bat has attacked you. If your health is too low you need to equip the armor in your bag. Otherwise the Winged Bat will end your adventure and JS will be forever lost.
+
+`Hint: You have the 'armor' in your bag. Put the armor in your armorEquip`;
+
+### Oooh Drops!!!!
+If you have defeated the winged bat you notice he has dropped some items. A total of 6 items total these items can only be used by a specific class of hero. You must only take one and move on with your adventure.
+
+Do not assume your class. We will be watching. And don't just change your class so you can pick up a certain item either.
+
+* `Archer`: Ichaival
+* `Swordsman`: Excalibur
+* `Sorcerous`: Staff of Origin
+* `thief`: Fragarach
+* `alchemist`: Philosopher Stone
+* `No Class`: Some Magical Rock
+
+`Hint: There are 6 conditions but you only need to handle the first 5 and the last will be by default.`
+
+---
+## The Dragon Has Descended
+He has been watching you. He's watched you grow, struggle, and persevere. Now he see's you as a threat to his plans. You must be destroyed. Depending on your actions to date, only luck will be what determines how this tale will end. Defeat the dragon deity who commands time itself. Oh hero. FIGHT!!!!!!!
+
+![Dragon](http://bestanimations.com/Fantasy/Dragons/dragon-animated-gif-74.gif)
--- character.js
+++ character.js
@@ -1,40 +1,83 @@
-// 
-// // gather base information from story or through instruction
-// var firstName = 'Jon';
-// var lastName = 'Cornfield';
-// var age = 10;
-// var bag = ['armor', 'map'];
-//
-// var heroClass = "No Class";// swordsman sourcerous archer thief alchemist
-//
-//
-// // build hero based on information gathered
-// var hero = {
-//   name: firstName + ' ' + lastName,
-//   health: 10,
-//   age: age,
-//   bag: bag,
-//   armorEquip: null,
-//   weaponEquip: null
-// };
-//
-// // Your health is dependant on your age / 2. If it is lower than 10 you need to equip the armor.
-// if (hero.health <= 10) {
-//   hero.armorEquip = bag[0];
-// }
-//
-// // Multi conditional
-//
-// if (heroClass == "swordsman") {
-//   hero.weaponEquip = "Excaliber";
-// } else if (heroClass == "sourcerous") {
-//   hero.weaponEquip = "Staff of Origin";
-// } else if (heroClass == "archer") {
-//   hero.weaponEquip = "Ichaival";
-// } else if (heroClass == "thief") {
-//   hero.weaponEquip = "Fragarach";
-// } else if (heroClass == "alchemist") {
-//   hero.weaponEquip = "Philosipher Stone";
-// } else {
-//   hero.weaponEquip = "Some Magical Rock";
-// }
+/**
+ * create a variable called `firstName` with your first name as a value.
+ * @type {String}
+ */
+var firstName = 'Jon';
+
+/**
+ * create a variable called `lastName` with your first name as a value.
+ * @type {String}
+ */
+var lastName = 'Cornfield';
+
+/**
+ * create a variable called `age` with your current age.
+ * @type {Number}
+ */
+var age = 10;
+
+/**
+ * create a variable called `bag` with 2 items: `armor` and `map`.
+ * @type {Array}
+ */
+var bag = ['armor', 'map'];
+
+/**
+ * create a variable called `heroClass` that will be one of 6 options.
+ * You will determine what class you want to be and work with.
+ * 'Archer', 'Swordsman', 'Sorcerous', 'thief', 'alchemist', or 'No Class'
+ * @type {String}
+ */
+var heroClass = "No Class";// swordsman sourcerous archer thief alchemist
+
+/**
+ * Create a variable called hero
+ * @type {Object}
+ * @property `name`: Your first and last name separated with a space.
+ * @property `health`: Set the value of your heath to be half your age.
+ * @property `age`: Your age.
+ * @property `bag`: Your bag that you created earlier.
+ * @property `armorEquip`: Set it to `null` not in quotes.
+ * @property `weaponEquip`: Set it to `null` not in quotes.
+ * @property `heroClass`: Set it to your hero class.
+ */
+var hero = {
+  name: firstName + ' ' + lastName,
+  health: (age / 2),
+  age: age,
+  bag: bag,
+  armorEquip: null,
+  weaponEquip: null,
+  heroClass: heroClass
+};
+
+/**
+ * Create a condition that will equip the 'armor' from the bag
+ * if your health is less than or equal to 10.
+ */
+if (hero.health <= 10) {
+  hero.armorEquip = bag[0];
+}
+
+/**
+ * Create conditions that will give you a spacific item based on what your class is.
+ * `Archer`: Ichaival
+ * `Swordsman`: Excalibur
+ * `Sorcerous`: Staff of Origin
+ * `thief`: Fragarach
+ * `alchemist`: Philosopher Stone
+ * `No Class`: Some Magical Rock
+ */
+if (hero.heroClass == "swordsman") {
+  hero.weaponEquip = "Excaliber";
+} else if (hero.heroClass == "sorcerous") {
+  hero.weaponEquip = "Staff of Origin";
+} else if (hero.heroClass == "archer") {
+  hero.weaponEquip = "Ichaival";
+} else if (hero.heroClass == "thief") {
+  hero.weaponEquip = "Fragarach";
+} else if (hero.heroClass == "alchemist") {
+  hero.weaponEquip = "Philosopher Stone";
+} else {
+  hero.weaponEquip = "Some Magical Rock";
+}
