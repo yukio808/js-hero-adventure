c5370384c68af4d707ba72ab69e31695c0b9a949
yukio yamamoto <aj.yamamoto@me.com>
2016-10-20 18:40:34 -1000

    sanitized version for students

--- character.js
+++ character.js
@@ -2,25 +2,25 @@
  * create a variable called `firstName` with your first name as a value.
  * @type {String}
  */
-var firstName = 'Jon';
+
 
 /**
  * create a variable called `lastName` with your first name as a value.
  * @type {String}
  */
-var lastName = 'Cornfield';
+
 
 /**
  * create a variable called `age` with your current age.
  * @type {Number}
  */
-var age = 10;
+
 
 /**
  * create a variable called `bag` with 2 items: `armor` and `map`.
  * @type {Array}
  */
-var bag = ['armor', 'map'];
+
 
 /**
  * create a variable called `heroClass` that will be one of 6 options.
@@ -28,7 +28,6 @@ var bag = ['armor', 'map'];
  * 'Archer', 'Swordsman', 'Sorcerous', 'thief', 'alchemist', or 'No Class'
  * @type {String}
  */
-var heroClass = "No Class";// swordsman sourcerous archer thief alchemist
 
 /**
  * Create a variable called hero
@@ -41,23 +40,13 @@ var heroClass = "No Class";// swordsman sourcerous archer thief alchemist
  * @property `weaponEquip`: Set it to `null` not in quotes.
  * @property `heroClass`: Set it to your hero class.
  */
-var hero = {
-  name: firstName + ' ' + lastName,
-  health: (age / 2),
-  age: age,
-  bag: bag,
-  armorEquip: null,
-  weaponEquip: null,
-  heroClass: heroClass
-};
+
 
 /**
  * Create a condition that will equip the 'armor' from the bag
  * if your health is less than or equal to 10.
  */
-if (hero.health <= 10) {
-  hero.armorEquip = bag[0];
-}
+
 
 /**
  * Create conditions that will give you a spacific item based on what your class is.
@@ -68,16 +57,3 @@ if (hero.health <= 10) {
  * `alchemist`: Philosopher Stone
  * `No Class`: Some Magical Rock
  */
-if (hero.heroClass == "swordsman") {
-  hero.weaponEquip = "Excaliber";
-} else if (hero.heroClass == "sorcerous") {
-  hero.weaponEquip = "Staff of Origin";
-} else if (hero.heroClass == "archer") {
-  hero.weaponEquip = "Ichaival";
-} else if (hero.heroClass == "thief") {
-  hero.weaponEquip = "Fragarach";
-} else if (hero.heroClass == "alchemist") {
-  hero.weaponEquip = "Philosopher Stone";
-} else {
-  hero.weaponEquip = "Some Magical Rock";
-}
